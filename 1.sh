f6a19b103fd6ae3f2dc5c0552e8a8662795dad02
RominaKavi <Romishkavi@gmail.com>
2021-06-10 13:32:26 +0430

    oslab9

--- 1.sh
+++ 1.sh
@@ -1,8 +1,8 @@
 whatis chown
 chown (1)            - change file owner and group
-chown -c
 chown -f
 chown -R
+chown -c
 
 whatis gpasswd
 gpasswd (1)          - administer /etc/group and /etc/gshadow
